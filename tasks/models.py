df5c908183403f14eeb7f0d7e9b3aacb5b9e7282
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-03 17:51:14 +0300

    Create Tag and Task models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True, related_name="tags")
+
+    class Meta:
+        ordering = [
+            "done",
+            "-datetime",
+        ]
