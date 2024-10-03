5880baaa67c7c46eace8394454ae2c17a2fa7303
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-03 20:12:39 +0300

    Add ToggleCompleteView, add toggle-complete url

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,11 @@ from tasks import views
 urlpatterns = [
     path("", views.TaskListView.as_view(), name="index"),
     path("tags/", views.TagListView.as_view(), name="tags-list"),
+    path(
+        "tasks/<int:pk>/toggle-complete",
+        views.ToggleCompleteView.as_view(),
+        name="toggle-complete"
+    ),
 ]
 
 app_name = "tasks"
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+from django.views import View
 from django.views.generic import ListView
 
 from tasks.models import Task, Tag
@@ -13,3 +16,12 @@ class TagListView(ListView):
     model = Tag
     template_name = "tasks/tags_list.html"
     context_object_name = "tags"
+
+
+class ToggleCompleteView(View):
+    @staticmethod
+    def get(request, pk, *args, **kwargs):
+        task = get_object_or_404(Task, pk=pk)
+        task.done = not task.done
+        task.save()
+        return redirect(reverse("tasks:index"))
