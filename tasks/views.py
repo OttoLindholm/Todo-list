805121cf6d4fe7d28f25e2dce62ae889c74c71ee
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-03 19:50:32 +0300

    Add TagsListView and empty tags_list.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,15 @@
 from django.views.generic import ListView
 
-from tasks.models import Task
+from tasks.models import Task, Tag
 
 
 class TaskListView(ListView):
     model = Task
     template_name = "tasks/task_list.html"
     context_object_name = "tasks"
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = "tasks/tags_list.html"
+    context_object_name = "tags"
