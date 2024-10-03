80ca02002712e1527afb116cc9f8524ad3eaec63
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-03 20:37:39 +0300

    Add task create view, add urls for create

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,7 @@ from tasks import views
 
 urlpatterns = [
     path("", views.TaskListView.as_view(), name="index"),
+    path("tasks/create/", views.TaskCreateView.as_view(), name="task-create"),
     path("tags/", views.TagListView.as_view(), name="tags-list"),
     path(
         "tasks/<int:pk>/toggle-complete",
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
+from tasks.forms import TaskForm
 from tasks.models import Task, Tag
 
 
@@ -10,6 +11,12 @@ class TaskListView(ListView):
     model = Task
     template_name = "tasks/task_list.html"
     context_object_name = "tasks"
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:index")
 
 
 class TagListView(ListView):
