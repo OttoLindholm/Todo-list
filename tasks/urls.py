a0f89ecde43349e4dfcd028baf56f3f68f94edc6
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-03 19:52:41 +0300

    Add url for TagListView (also add link to sidebar)

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,6 +4,7 @@ from tasks import views
 
 urlpatterns = [
     path("", views.TaskListView.as_view(), name="index"),
+    path("tags/", views.TagListView.as_view(), name="tags-list"),
 ]
 
 app_name = "tasks"
