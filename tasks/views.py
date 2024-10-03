109c8bee6270677d98d833ebc3b8fe0d829f8c64
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-03 19:20:17 +0300

    Change template path in TaskListView, change bootstrap version to 5 in base.html, add content to task_list.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 
 from tasks.models import Task
@@ -6,5 +5,5 @@ from tasks.models import Task
 
 class TaskListView(ListView):
     model = Task
-    template_name = "templates/tasks/task_list.html"
+    template_name = "tasks/task_list.html"
     context_object_name = "tasks"
