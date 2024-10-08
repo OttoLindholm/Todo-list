233fd27b3ae2e2d5f8af1198fab56be365031c3d
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-08 20:38:11 +0300

    Refactor code

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,8 +7,16 @@ urlpatterns = [
     path("tasks/create/", views.TaskCreateView.as_view(), name="task-create"),
     path("tags/", views.TagListView.as_view(), name="tags-list"),
     path("tags/create/", views.TagCreateView.as_view(), name="tag-create"),
-    path("tags/<int:pk>/update/", views.TagUpdateView.as_view(), name="tag-update"),
-    path("tags/<int:pk>/delete/", views.TagDeleteView.as_view(), name="tag-delete"),
+    path(
+        "tags/<int:pk>/update/",
+        views.TagUpdateView.as_view(),
+        name="tag-update"
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        views.TagDeleteView.as_view(),
+        name="tag-delete"
+    ),
     path(
         "tasks/<int:pk>/toggle-complete/",
         views.ToggleCompleteView.as_view(),
